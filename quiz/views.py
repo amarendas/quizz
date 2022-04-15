61f21bbca90ed0535706edecd7bcae2f7bc2be2f
crossconnect <amaren.das@gmail.com>
2022-04-15 22:20:46 +0530

    Subject list added

--- quiz/views.py
+++ quiz/views.py
@@ -3,6 +3,8 @@ from .models import Quiz_Question,Author, Subject
 
 def index(request):
     question_list =Quiz_Question.objects.all()
+    topic_list=Subject.objects.all()
+    
     q_nos=Quiz_Question.objects.count()
-    context={'q_list':question_list,"q_nos":q_nos}
+    context={'q_list':question_list,"q_nos":q_nos,'topics':topic_list}
     return render(request,'index.html', context)
