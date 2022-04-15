528fbcf173b7ca84d6346ac5664bab469c042743
crossconnect <amaren.das@gmail.com>
2022-04-15 21:38:47 +0530

    chadded no of questions

--- quiz/views.py
+++ quiz/views.py
@@ -3,5 +3,6 @@ from .models import Quiz_Question,Author, Subject
 
 def index(request):
     question_list =Quiz_Question.objects.all()
-    context={'q_list':question_list}
+    q_nos=Quiz_Question.objects.count()
+    context={'q_list':question_list,"q_nos":q_nos}
     return render(request,'index.html', context)
