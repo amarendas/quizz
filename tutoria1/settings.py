e16eb918b3ab46f5272ef234049827ae770cf3b3
amarendas <amaren.das@gmail.com>
2022-05-04 00:29:10 +0530

    Settings.py added authorization key comment

--- tutoria1/settings.py
+++ tutoria1/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
+
+# Keys specific to dropbox for storing image on dropbox
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN='sl.BG75r1idlcx3LKCv46tcKNwSZg9U_EsrSOzg2Vvnia7zaeGH0O8AKaTNGiPLL7xC1_pii3Dyg5iJuFc5widrx_le_H-Nuzrn9U07tX1KF67xfX-b7hdA-EC5WqPWCU0EV0A4jEs'
 
